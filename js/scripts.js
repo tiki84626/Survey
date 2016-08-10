ac2653e642ca4ec33eafb8bfd0d4631d993c14a9
Andrew Malkin and Marta Sanchez <andrew@>
2016-08-10 15:47:35 -0700

    fix condition statements in our javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var name = $("#name").val();
     var dob = $("#dob").val();
-    var favFood = $("#fav-food").val();
+    var favFood = parseInt($("#fav-food").val());
     var color = $("#color").val();
     var favMusic = $("input:radio[name=fav-music]:checked").val();
 
@@ -20,11 +20,11 @@ $(document).ready(function() {
       $("#food").append(img);
     }
 
-    if (favFood == 1) {
+    if (favFood === 1) {
       show_pic("img/american.jpg");
-    } else if (favFood == 2) {
+    } else if (favFood === 2) {
       show_pic("img/french.jpg");
-    } else if (favFood == 3) {
+    } else if (favFood === 3) {
       show_pic("img/italian.jpg");
     } else {
       show_pic("img/indian.jpg");
@@ -36,13 +36,13 @@ $(document).ready(function() {
       $("#music").append(img);
     }
 
-    if (favMusic == "rock") {
+    if (favMusic === "rock") {
       show_image("img/rock.jpg");
-    } else if (favMusic == "jazz") {
+    } else if (favMusic === "jazz") {
       show_image("img/jazz.jpg");
-    } else if (favMusic == "hip-hop") {
+    } else if (favMusic === "hip-hop") {
       show_image("img/hip-hop.jpg");
-    } else if (favMusic == "country") {
+    } else if (favMusic === "country") {
       show_image("img/country.jpeg");
     } else {
       show_image("img/blues.jpg")
